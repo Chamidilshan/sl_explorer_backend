8c17564e35770426debbea050837a11d92226f86
Imashi2000 <imashinethmini88@gmail.com>
2024-01-31 14:37:49 +0530

    added day trips api

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 const hotelsRouter = require("./src/v1/routes/hotels/hotels.router");
 const roundTripsRouter = require("./src/v1/routes/roundTrips/roundTrips.router");
+const dayTripsRouter=require("./src/v1/routes/dayTrips/dayTrips.router");
  
 app.use(express.json());     
 
 app.use("/api/v1/hotels", hotelsRouter); 
 app.use("/api/v1/roundTrips", roundTripsRouter); 
+app.use("/api/v1/dayTrips",dayTripsRouter);
 
 const listener = app.listen(process.env.PORT || 5000, () => {
     console.log('App is listening on port ' + listener.address().port)
